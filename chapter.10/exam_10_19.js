f44c9ab12d151978a3bea857c3e64417ae9b5c6d
고세종 <>
2022-01-14 20:03:47 +0900

    chapter.10 review

---
+++ chapter.10/exam_10_19.js
@@ -0,0 +1,14 @@
+var x = 1, y = 2;
+
+var obj = {
+    x: x,
+    y: y
+};
+
+console.log(obj);
+
+let x = 1, y = 2;
+
+const obj = {x, y};
+
+console.log(obj); // {x: 1, y: 2}
---
+++ chapter.10/exam_10_6.js
@@ -0,0 +1,7 @@
+var obj = {};
+var key = 'hello';
+
+obj[key] = 'world';
+
+console.log(obj);
+console.log(obj.hello);
